3ae5770f7693871efcc4c2fd067f2f9914183563
thakaredipali <dipali.thakare@joshsoftware.com>
2026-07-20 15:52:50 +0530

    test: add unit tests for allow backup audit

---
+++ test/audit_engine_test.dart
@@ -0,0 +1,17 @@
+import 'package:test/test.dart';
+
+void main() {
+  group('AuditEngine', () {
+    test('runs all registered audits', () {
+      // TODO: Implement
+    });
+
+    test('returns empty result when no audits are registered', () {
+      // TODO: Implement
+    });
+
+    test('returns results from all audits', () {
+      // TODO: Implement
+    });
+  });
+}
---
+++ test/audits/android/allow_backup_audit_test.dart
@@ -0,0 +1,29 @@
+import 'package:test/test.dart';
+
+void main() {
+  group('AllowBackupAudit', () {
+    test('returns issue when allowBackup is true', () async {
+      // Arrange
+
+      // Act
+
+      // Assert
+    });
+
+    test('returns no issue when allowBackup is false', () async {
+      // Arrange
+
+      // Act
+
+      // Assert
+    });
+
+    test('returns no issue when AndroidManifest.xml is missing', () async {
+      // Arrange
+
+      // Act
+
+      // Assert
+    });
+  });
+}
