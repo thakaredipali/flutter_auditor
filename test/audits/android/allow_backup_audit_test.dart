e63f4d78a5a6c2ac83c04fefa49c1f14b8c02ac5
thakaredipali <dipali.thakare@joshsoftware.com>
2026-07-27 17:16:16 +0530

    feat: add unit test

--- test/audits/android/allow_backup_audit_test.dart
+++ test/audits/android/allow_backup_audit_test.dart
@@ -1,29 +1,60 @@
+import 'dart:io';
+
+import 'package:flutter_auditor/audits/android/allow_backup_audit.dart';
+import 'package:flutter_auditor/models/project_context.dart';
+import 'package:flutter_auditor/models/severity.dart';
 import 'package:test/test.dart';
+
+import '../../helpers/test_helper.dart';
 
 void main() {
   group('AllowBackupAudit', () {
+    late AllowBackupAudit audit;
+
+    setUp(() {
+      audit = AllowBackupAudit();
+    });
+
     test('returns issue when allowBackup is true', () async {
-      // Arrange
+      final context = await createProjectContext(
+        manifestContent: '''
+<manifest xmlns:android="http://schemas.android.com/apk/res/android">
+    <application android:allowBackup="true">
+    </application>
+</manifest>
+''',
+      );
 
-      // Act
+      final result = await audit.run(context);
 
-      // Assert
+      expect(result.issues.length, 1);
+      expect(result.issues.first.severity, Severity.high);
     });
 
     test('returns no issue when allowBackup is false', () async {
-      // Arrange
+      final context = await createProjectContext(
+        manifestContent: '''
+<manifest xmlns:android="http://schemas.android.com/apk/res/android">
+    <application android:allowBackup="false">
+    </application>
+</manifest>
+''',
+      );
 
-      // Act
+      final result = await audit.run(context);
 
-      // Assert
+      expect(result.issues, isEmpty);
     });
 
     test('returns no issue when AndroidManifest.xml is missing', () async {
-      // Arrange
+      final tempDir = await Directory.systemTemp.createTemp(
+        'flutter_auditor_test',
+      );
+      final context = ProjectContext(rootDirectory: tempDir);
 
-      // Act
+      final result = await audit.run(context);
 
-      // Assert
+      expect(result.issues, isEmpty);
     });
   });
 }
--- test/audits/android/debuggable_audit_test.dart
+++ test/audits/android/debuggable_audit_test.dart
@@ -1,29 +1,60 @@
+import 'dart:io';
+
+import 'package:flutter_auditor/audits/android/debuggable_audit.dart';
+import 'package:flutter_auditor/models/project_context.dart';
+import 'package:flutter_auditor/models/severity.dart';
 import 'package:test/test.dart';
+
+import '../../helpers/test_helper.dart';
 
 void main() {
   group('DebuggableAudit', () {
+    late DebuggableAudit audit;
+
+    setUp(() {
+      audit = DebuggableAudit();
+    });
+
     test('returns issue when debuggable is true', () async {
-      // Arrange
+      final context = await createProjectContext(
+        manifestContent: '''
+<manifest xmlns:android="http://schemas.android.com/apk/res/android">
+    <application android:debuggable="true">
+    </application>
+</manifest>
+''',
+      );
 
-      // Act
+      final result = await audit.run(context);
 
-      // Assert
+      expect(result.issues.length, 1);
+      expect(result.issues.first.severity, Severity.high);
     });
 
     test('returns no issue when debuggable is false', () async {
-      // Arrange
+      final context = await createProjectContext(
+        manifestContent: '''
+<manifest xmlns:android="http://schemas.android.com/apk/res/android">
+    <application android:debuggable="false">
+    </application>
+</manifest>
+''',
+      );
 
-      // Act
+      final result = await audit.run(context);
 
-      // Assert
+      expect(result.issues, isEmpty);
     });
 
     test('returns no issue when AndroidManifest.xml is missing', () async {
-      // Arrange
+      final tempDir = await Directory.systemTemp.createTemp(
+        'flutter_auditor_test',
+      );
+      final context = ProjectContext(rootDirectory: tempDir);
 
-      // Act
+      final result = await audit.run(context);
 
-      // Assert
+      expect(result.issues, isEmpty);
     });
   });
 }
